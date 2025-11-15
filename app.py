f147c381a4a6c651e120bfda341f51ff8a4ec275
julien-lambert <perso.julien.lambert@gmail.com>
2025-11-15 16:23:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -842,8 +842,8 @@ import os
 
 if __name__ == "__main__":
 # <<<<<<< HEAD
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
 # =======
-    port = int(os.environ.get("PORT", 5001))  # 5001 en local, PORT imposé sur Render
-    app.run(host="0.0.0.0", port=port, debug=True)
+#    port = int(os.environ.get("PORT", 5001))  # 5001 en local, PORT imposé sur Render
+#    app.run(host="0.0.0.0", port=port, debug=True)
